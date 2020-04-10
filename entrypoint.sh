cd0827a5165c175759306addc2448013b13697db
sven <sven@so36.net>
2020-04-10 13:46:58 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
-echo "::set-output name=status::Hello Cool"
+#echo "Hello $1"
+#time=$(date)
+#echo "::set-output name=time::$time"
+#echo "::set-output name=status::Hello Cool"
+FILE=test.log
+
+if [ -s "$FILE" ]; then
+    echo ::set-output name=status::failure
+    exit 1
+fi
