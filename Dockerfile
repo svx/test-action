f725b860d911c8c1c5e685de324f0f7336bcebd2
sven <sven@so36.net>
2020-04-11 14:51:43 +0200

    test2

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 # Container image that runs your code
 FROM alpine:3.11
 
+RUN apk --no-cache add \
+    curl \
+    bash \
+    jq
+
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,10 @@ if [ -s "$FILE" ]; then
     echo ::set-output name=status::failure
     #echo ::set-output name=output::$(cat test.log)
     cat test.log
+    jq -nc '{"body": "test comment"}' | \
+        curl -sL  -X POST -d @- \
+        -H "Content-Type: application/json" \
+        -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" \
+        "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/comments"
+    exit 1
 fi
