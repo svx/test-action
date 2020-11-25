c0f18e0040a589752a91a13348a839aafeb86ad4
sven <sven@so36.net>
2020-11-25 13:51:20 +0100

    test 8

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,11 +20,8 @@ exit_code=0
 if [ ! -z "$tw_lines" ]; then
   echo ::set-output name=status::failure
   #echo -e "\n***** Lines containing trailing whitespace *****\n"
-  #echo -e "${tw_lines[@]}"
-  PAYLOAD=$(echo '{}' | jq --arg body "${tw_lines[@]}" '.body = $body')
-  COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-  echo "Commenting on PR $COMMENTS_URL"
-  curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL"
+  echo -e "${tw_lines[@]}"
+  
 
   #echo -e "\n\nFailed!\n"
   exit_code=1
